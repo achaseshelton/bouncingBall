42897f6aeccaa9fd3c1aa8f9a649ab017e7c6078
austen.shelton <austen.shelton@gmail.com>
2021-09-16 12:50:03 -0400

    script edits

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ Ball.prototype.collisionDetect = function () {
 let balls = [];
 
 while(balls.length < 25) {
-  let size = random(10,20);
+  const size = random(10,20);
   let ball = new Ball(
     random(0 + size,width - size),
     random(0 + size,height -size),
